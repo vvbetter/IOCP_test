d925f065765da3ed89874f1bedbfc1c87b42ab5a
vvbetter <424247407@qq.com>
2018-07-02 16:10:34 +0800

    proto

--- CHEN_Test/NetService.cpp
+++ CHEN_Test/NetService.cpp
@@ -4,8 +4,10 @@
 #include "CLog.h"
 #include "CS_LockGuide.h"
 #include "proto_source/Game60_FishingMessage.pb.h"
+#include <ws2tcpip.h>
 
 using namespace com::game::proto;
+#define SET_ANSWER(X) ((X) | 0x80000000)
 
 NetService::NetService()
 {
@@ -20,12 +22,12 @@ NetService::~NetService()
 
 const HANDLE NetService::GetHanle()
 {
-	for (auto it = m_sockList.rbegin(); it != m_sockList.rend(); ++it)
+	for (auto it = m_IoDataMap.rbegin(); it != m_IoDataMap.rend(); ++it)
 	{
-		if (it->RegState == false)
+		if (it->second->pSocketData->RegState == false)
 		{
-			it->RegState = true;
-			return (HANDLE)it->Socket;
+			it->second->pSocketData->RegState = true;
+			return (HANDLE)it->second->pSocketData->Socket;
 		}
 	}
 	TRANSLOG("NetService Get Unkown Handle");
@@ -54,48 +56,85 @@ bool NetService::Init()
 	InitializeCriticalSection(&m_cs);
 	return true;
 }
-
-SOCKET NetService::RegesterNewSocket(CreateSocketType type, ULONG localIP, USHORT port)
+#define  UDP_SOCKET_PORT_START 50000
+SOCKET NetService::RegesterNewSocket(INT64 uid, CreateSocketType type, ULONG localIP, USHORT port)
 {
-	CreateSocketData sData;
-	if (CreateSocket(type, localIP, port, sData) == false)
+	CreateSocketData*  sData = new CreateSocketData();
+	if (CreateSocket(type, localIP, port, *sData) == false)
 	{
 		TRANSLOG("NetService Create New Socket Error");
 		return INVALID_SOCKET;
 	}
+	SOCKADDR_IN ser_addr;
+	ser_addr.sin_port = htons(UDP_SOCKET_PORT_START + uid);
+	ser_addr.sin_family = AF_INET;
+
+	IN_ADDR addr;
+	int d = InetPton(AF_INET, L"192.168.0.89", &addr);
+	if (1 != d)
+	{
+		closesocket(sData->Socket);
+		cout << "InetPton error " << WSAGetLastError() << endl;
+		return -1;
+	}
+	ser_addr.sin_addr = addr;
+	int ret = connect(sData->Socket, (sockaddr*)&ser_addr, sizeof(SOCKADDR_IN));
+	if (ret == SOCKET_ERROR)
+	{
+		cout << "connect error !" << endl;
+		closesocket(sData->Socket);
+		return INVALID_SOCKET;
+	}
+	NetIoData* pIodata = new NetIoData();
+	memset(pIodata, 0, sizeof(NetIoData));
+	pIodata->pSocketData = sData;
+	pIodata->recvID = 0;
+	pIodata->sendID = 0;
+	pIodata->wsabuf.buf = pIodata->buffer;
+	pIodata->wsabuf.len = IOCP_BUFFER_SIZE;
 	{
 		AUTO_LOCKER(m_cs);
-		m_sockList.push_back(sData);
+		m_IoDataMap[uid] = pIodata;
 	}
 	RegIOHandle();
-	return sData.Socket;
+	return sData->Socket;
 }
 
 bool NetService::RemoveSocket(SOCKET s)
 {
-	for (auto it = m_sockList.begin(); it != m_sockList.end(); ++it)
-	{
-		if (it->Socket == s)
-		{
-			closesocket(s);
-			{
-				AUTO_LOCKER(m_cs);
-				it = m_sockList.erase(it);
-			}
-			return true;
-		}
-	}
+	//for (auto it = m_sockMap.begin(); it != m_sockMap.end(); ++it)
+	//{
+	//	if (it->second.Socket == s)
+	//	{
+	//		closesocket(s);
+	//		{
+	//			AUTO_LOCKER(m_cs);
+	//			it = m_sockMap.erase(it);
+	//		}
+	//		return true;
+	//	}
+	//}
 	return false;
 }
-struct NetIoData :public PerIocpData
-{
-	WSABUF wsabuf;
-	UINT operatro;
-};
 
 bool NetService::CheckUid(INT64 uid)
 {
+	auto it = m_IoDataMap.find(uid);
+	if (it == m_IoDataMap.end())
+	{
+		cout << "uid :" << uid << "checkUid error" << endl;
+		return false;
+	}
+	it->second->OPT = 1;
 	ReqEnterFishServerMessage msg;
 	msg.set_playeronlyid(uid);
-	return false;
+	CHAR* pBuff = it->second->buffer;
+	USHORT msgID = Protos_Game60Fishing::ReqEnterFishServer;
+	memcpy_s(pBuff, 2, &msgID, 2);
+	USHORT msgLen = msg.ByteSize();
+	memcpy_s(pBuff + 2, 2, &msgLen, 2);
+	msg.SerializeToArray(pBuff + 4, msg.ByteSize());
+	DWORD bytsSend;
+	WSASend(it->second->pSocketData->Socket, &it->second->wsabuf, msgLen + 4, &bytsSend, 0, &it->second->overlapped, NULL);
+	return true;
 }
--- CHEN_Test/NetService.h
+++ CHEN_Test/NetService.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "Interface.h"
-#include <list>
+#include <map>
 #include <process.h>
+struct NetIoData :public PerIocpData
+{
+	CreateSocketData* pSocketData;
+	WSABUF wsabuf;
+	UINT OPT;
+	UINT sendID;
+	UINT recvID;
+};
+
 class NetService : public IOCPHanlder
 {
 public:
@@ -13,12 +22,12 @@ public: //interface
 	virtual bool RegIOHandle();
 public:
 	bool Init();
-	SOCKET RegesterNewSocket(CreateSocketType type, ULONG localIP, USHORT port);
+	SOCKET RegesterNewSocket(INT64 uid, CreateSocketType type, ULONG localIP, USHORT port);
 	bool RemoveSocket(SOCKET s);
 	//实现功能
 	bool CheckUid(INT64 uid);
 private:
 	CRITICAL_SECTION m_cs;
-	list<CreateSocketData> m_sockList;	//创建完成的socket列表
+	map<INT64, NetIoData*> m_IoDataMap;	//创建完成的socket列表
 };
 
